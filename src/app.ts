3d1679428cb5e2a3c9b16653e7501555ef957146
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2025-05-28 21:09:04 +0530

    Add more code

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
-const word = 'hello';
+import https from 'node:https';
 
-console.log(word);
+const options = {
+  hostname: 'example.com',
+  port: 443,
+  path: '/todos',
+  method: 'GET',
+};
+
+const req = https.request(options, (res) => {
+  console.log(`statusCode: ${res.statusCode?.toString() ?? 'N/A'}`);
+
+  res.on('data', (d: string | Uint8Array) => {
+    process.stdout.write(d);
+  });
+});
+
+req.on('error', (error) => {
+  console.error(error);
+});
+
+req.end();
